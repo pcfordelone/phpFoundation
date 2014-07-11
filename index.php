712d2043d03ba2cd451f55935e95249100a4ba64
PC Fordelone <pc@fordelone.com.br>
2014-07-11 15:48:05 -0300

    - Tentando utilizar rotas;

--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 
 <?php
 
-if (!isset($_GET['page']))
-    require_once ("home.php");
-else
-    require_once ($_GET['page'].".php");
+$dados_url  = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$rota = explode('/',$dados_url['path'],2);// essa variável virou um array pelo que pude perceber, correto? Queria colocar os dados contidos em $rota[1] na função abaixo rotas().
+
+$rotas = function($rota) use($rota) {
+    $rotasValidas = ['','home','empresa','produtos','servicos','contato'];
+    if (in_array($rota,$rotasValidas)) {
+        require_once($rota.".php");
+    } elseif (in_array($rota,'')) {
+        require_once("404.php");
+    } else {
+        require_once("home.php");
+    }
+};
+
+
 ?>
 
 <?php require_once "boxs.php" ?>
